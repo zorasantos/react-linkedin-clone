f61320587e58002a54b2b4d9d980134862ff0696
zoranildosantos <zoranildo22@gmail.com>
2020-08-31 22:04:49 -0300

    Updates

---
+++ precache-manifest.f0e420bdd70c6db39d1cd5fbd0c2337b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ba16a18f0b039f22708e7669054688",
+    "url": "/react-linkedin-clone/index.html"
+  },
+  {
+    "revision": "3ecaf6283b93932128ac",
+    "url": "/react-linkedin-clone/static/js/2.3035b28c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-linkedin-clone/static/js/2.3035b28c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba16ca9b48cd930ec645",
+    "url": "/react-linkedin-clone/static/js/main.b62624ea.chunk.js"
+  },
+  {
+    "revision": "405b38cb5bc2a339bbb8",
+    "url": "/react-linkedin-clone/static/js/runtime-main.fdbceb90.js"
+  }
+]);
